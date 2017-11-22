05119f82ad06069e168c07b3764ee2392a902232
damienhervieu <damien.hervieu@ynov.com>
2017-11-22 17:26:29 +0100

    Ajout de quelques fonctionnalités

---
+++ server.js
@@ -0,0 +1,20 @@
+var io = require('socket.io');
+var express = require('express');
+var path = require('path');
+
+var routes = require('./routes/routes');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname + 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', routes);
+
+app.set('port', (process.env.PORT || 8080));
+
+app.listen(app.get('port'), function(){
+	console.log('Serveur démarré sur le port ' + app.get('port'));
+});
