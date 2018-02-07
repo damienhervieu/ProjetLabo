ef523071667340fd90d7878eb13da73463dd66de
damienhervieu <damien.hervieu@ynov.com>
2018-02-07 17:29:42 +0100

    Players data test

--- app.js
+++ app.js
@@ -11,7 +11,33 @@ var server = http.createServer(function(req, res){
 var io = require('socket.io').listen(server);
 var ent = require('ent');
 
-io.sockets.on('connection', function (socket){
+io.on('connection', function(socket){
+	socket.on('newplayer', function(){
+		socket.player = {
+			id: server.lastPlayerID++,
+			x: randomInt(0, 3500),
+			y: randomInt(0, 3500)
+		}
+		socket.emit('allplayers', getAllPlayers());
+		socket.broadcast.emit('newplayer', socket.player);
+	});
+});
+
+function getAllPlayers(){
+	var players = [];
+	Object.keys(io.sockets.connected).forEach(function(socketID){
+		var player = io.sockets.connected[socketID].player;
+		if (player) {
+			players.push(player);
+		}
+	});
+}
+
+function randomInt (lowest, highest) {
+	return Math.floor(Math.random() * (highest - lowest) + lowest);
+}
+
+/*io.sockets.on('connection', function (socket){
 		socket.emit('message', 'Bienvenue sur Tron.io !!!');
 
 	socket.on('new_user', function(pseudo){
@@ -24,6 +50,6 @@ io.sockets.on('connection', function (socket){
 		message = ent.encode(message);
 		socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
 	});
-});
+});*/
 
 server.listen(8080);
